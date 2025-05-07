665e368bd10570816e0ad5ddc3ed42cb3484fec8
Farhat <farhatkhabache@gmail.com>
2025-05-07 15:59:30 +0200

    fix: amélioration formulaire login avec icône mot de passe

--- frontend/src/utils/configs/FormFields.js
+++ frontend/src/utils/configs/FormFields.js
@@ -18,7 +18,6 @@ export const LOGIN_FIELDS = [
 export const REGISTER_FIELDS = [
     { label: "Nom", name: "nom", type: "text", id: "nom" },
     { label: "Prénom", name: "prenom", type: "text", id: "prenom" },
-    { label: "Date de naissance", name: "dateNaissance", type: "date", id: "dateNaissance" },
     { label: "Téléphone", name: "telephone", type: "tel", id: "telephone" },
     { label: "Adresse e-mail", name: "email", type: "email", id: "email" },
     { label: "Mot de passe", name: "password", type: "password", id: "password" }
