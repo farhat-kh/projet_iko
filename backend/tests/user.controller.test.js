dca988e4d2da4394d8a3ff86ba1455fab0372fdf
Farhat <farhatkhabache@gmail.com>
2025-05-15 23:18:13 +0200

    ajout tests pour deleteUser + sign dans useController

--- backend/tests/user.controller.test.js
+++ backend/tests/user.controller.test.js
@@ -1,3 +1,9 @@
+// mock la fonction sendEmail pour eviter l'envoi de mail
+jest.mock("../services/nodemailer", () => ({
+  sendEmail: jest.fn(() => Promise.resolve()),
+  sendVerificationEmail: jest.fn(() => Promise.resolve())
+}))
+
 const userController = require("../controllers/user.controller");
 const Users = require("../models/user.model");
 
@@ -20,3 +26,48 @@ describe("userController.getAllUsers", () => {
     expect(res.json).toHaveBeenCalledWith(mockUsers);
   });
 });
+
+
+
+test("sign renvoie une erreur si email non trouvé", async () => {
+  const req = { 
+    body: {
+      email: `emailInexistant@test.com`, password: "password"
+    }
+  }
+  const res = {}
+  const next = jest.fn()
+  jest.spyOn(Users, 'findOne').mockResolvedValue(null)
+  await userController.sign(req, res, next);
+  expect(next).toHaveBeenCalledWith(expect.objectContaining({
+    status: 404,
+    message: "user not found"
+  }))
+})
+
+
+test("deleteUser desactive un utilisateur avec succès", async () => {
+  const exempleUser = {
+    _id: "123",
+    isActive: true,
+    save: jest.fn()
+  }
+  const req = {
+    user: {
+      id: "123",  
+      role: "user"
+    },
+    params: {
+      id: "123"
+    }
+  }
+  const res = { status: jest.fn().mockReturnThis(), json: jest.fn() }
+  const next = jest.fn()
+  jest.spyOn(Users, 'findById').mockResolvedValue(exempleUser)
+  await userController.deleteUser(req, res, next);
+  expect(exempleUser.isActive).toBe(false)
+  expect(res.status).toHaveBeenCalledWith(200)
+  expect(res.json).toHaveBeenCalledWith({
+    message: "Compte désactivé avec succès"
+  })
+})
