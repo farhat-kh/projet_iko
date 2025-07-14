360c0d82c0af1c3865c368b7731f768f1c683449
Farhat <farhatkhabache@gmail.com>
2025-07-14 15:07:07 +0200

    Fix:corriger axiosInstance pour render

--- frontend/src/utils/services/AxiosInstance.js
+++ frontend/src/utils/services/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 
-const API_URL = 'http://localhost:8000/api'
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000/api'
 
 const AXIOS_INSTANCE = axios.create({
     baseURL: API_URL,
