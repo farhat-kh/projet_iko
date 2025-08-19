b36cc944d8f714a6374e32678e76de127ba40fcf
farhat-kh <farhat-kh@users.noreply.github.com>
2025-08-19 20:12:38 +0200

    fix: update package-lock.json and package.json to remove unused dependencies and improve configuration for jest

---
+++ backend/jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'node',
+  moduleDirectories: ['node_modules', '<rootDir>'],
+  testTimeout: 10000,
+  clearMocks: true,
+  collectCoverage: false,
+  verbose: true
+};
